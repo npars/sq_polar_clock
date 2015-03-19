131a94ac8a5383c309d23d91f440c607ca26478c
Neil Parsons <neilparsons@gmail.com>
2015-03-18 21:08:26 -0300

    Updated layers and paths to use arrays instead of individually declaring each variable.

--- src/sq_polar_clock.c
+++ src/sq_polar_clock.c
@@ -5,33 +5,23 @@
 #define INVERT_LAYER_COUNT 8
 #define POINT_COUNT 7
 
+#define HOUR   0
+#define MINUTE 1
+#define SECOND 2
+#define TIME_SIZE 3
+
 static Window *window;
 
-static Layer* second_layer;
-static Layer* minute_layer;
-static Layer* hour_layer;
+static Layer* time_layers[TIME_SIZE];
 static InverterLayer* invert_layers[INVERT_LAYER_COUNT];
 
-static int32_t second_angle;
-static int32_t minute_angle;
-static int32_t hour_angle;
+static int32_t time_angles[TIME_SIZE];
 
-static GPath *hour_path = NULL;
-static GPathInfo hour_info = {
-  .num_points = POINT_COUNT,
-  .points = (GPoint[POINT_COUNT]) {}
-};
-
-static GPath *minute_path = NULL;
-static GPathInfo minute_info = {
-  .num_points = POINT_COUNT,
-  .points = (GPoint[POINT_COUNT]) {}
-};
-
-static GPath *second_path = NULL;
-static GPathInfo second_info = {
-  .num_points = POINT_COUNT,
-  .points = (GPoint[POINT_COUNT]) {}
+static GPath* time_paths[TIME_SIZE];
+static GPathInfo time_infos[TIME_SIZE] = {
+  {.num_points = POINT_COUNT, .points = (GPoint[POINT_COUNT]) {}},
+  {.num_points = POINT_COUNT, .points = (GPoint[POINT_COUNT]) {}},
+  {.num_points = POINT_COUNT, .points = (GPoint[POINT_COUNT]) {}}
 };
 
 static void update_proc(Layer *layer, GContext *ctx, GPath *path, GPathInfo info, int32_t angle) {
@@ -77,35 +67,35 @@ static void update_proc(Layer *layer, GContext *ctx, GPath *path, GPathInfo info
 }
 
 static void second_update_proc(Layer *layer, GContext *ctx) {
-  update_proc(layer, ctx, second_path, second_info, second_angle);
+  update_proc(layer, ctx, time_paths[SECOND], time_infos[SECOND], time_angles[SECOND]);
 }
 
 static void minute_update_proc(Layer *layer, GContext *ctx) {
-  update_proc(layer, ctx, minute_path, minute_info, minute_angle);
+  update_proc(layer, ctx, time_paths[MINUTE], time_infos[MINUTE], time_angles[MINUTE]);
 }
 
 static void hour_update_proc(Layer *layer, GContext *ctx) {
-  update_proc(layer, ctx, hour_path, hour_info, hour_angle);
+  update_proc(layer, ctx, time_paths[HOUR], time_infos[HOUR], time_angles[HOUR]);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   if (units_changed & SECOND_UNIT) {
-    second_angle = TRIG_MAX_ANGLE * tick_time->tm_sec / 60;
-    layer_mark_dirty(second_layer); 
+    time_angles[SECOND] = TRIG_MAX_ANGLE * tick_time->tm_sec / 60;
+    layer_mark_dirty(time_layers[SECOND]); 
   }
  
   if (units_changed & MINUTE_UNIT) {
-    minute_angle = TRIG_MAX_ANGLE * tick_time->tm_min / 60;
-    layer_mark_dirty(minute_layer);
+    time_angles[MINUTE] = TRIG_MAX_ANGLE * tick_time->tm_min / 60;
+    layer_mark_dirty(time_layers[MINUTE]);
   }
 
   if (units_changed & HOUR_UNIT) {
     if (clock_is_24h_style()) {
-      hour_angle = TRIG_MAX_ANGLE * tick_time->tm_hour / 24;
+      time_angles[HOUR] = TRIG_MAX_ANGLE * tick_time->tm_hour / 24;
     } else {
-      hour_angle = TRIG_MAX_ANGLE * (tick_time->tm_hour % 12) / 12;
+      time_angles[HOUR] = TRIG_MAX_ANGLE * (tick_time->tm_hour % 12) / 12;
     }
-    layer_mark_dirty(hour_layer); 
+    layer_mark_dirty(time_layers[HOUR]);
   }
 }
 
@@ -144,41 +134,40 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  second_layer = layer_create((GRect) {
+  time_layers[SECOND] = layer_create((GRect) {
     .origin = { 0, 0 },
     .size = {bounds.size.w, bounds.size.h}});
-  second_path = gpath_create(&second_info);
-  layer_set_update_proc(second_layer, second_update_proc);
-  layer_add_child(window_layer, second_layer);
+  time_paths[SECOND] = gpath_create(&time_infos[SECOND]);
+  layer_set_update_proc(time_layers[SECOND], second_update_proc);
+  layer_add_child(window_layer, time_layers[SECOND]);
 
-  minute_layer = layer_create((GRect) {
+  time_layers[MINUTE] = layer_create((GRect) {
     .origin = {LAYER_WIDTH, LAYER_WIDTH},
     .size = {bounds.size.w - LAYER_WIDTH * 2, bounds.size.h - LAYER_WIDTH * 2}});
-  minute_path = gpath_create(&minute_info);
-  layer_set_update_proc(minute_layer, minute_update_proc);
-  layer_add_child(second_layer, minute_layer);
+  time_paths[MINUTE] = gpath_create(&time_infos[MINUTE]);
+  layer_set_update_proc(time_layers[MINUTE], minute_update_proc);
+  layer_add_child(time_layers[SECOND], time_layers[MINUTE]);
   int index = 0;
-  index = generate_inverter_layers(index, minute_layer);
+  index = generate_inverter_layers(index, time_layers[MINUTE]);
 
-  hour_layer = layer_create((GRect) {
+  time_layers[HOUR] = layer_create((GRect) {
     .origin = {LAYER_WIDTH, LAYER_WIDTH},
     .size = {bounds.size.w - LAYER_WIDTH * 4, bounds.size.h - LAYER_WIDTH * 4}});
-  hour_path = gpath_create(&hour_info);
-  layer_set_update_proc(hour_layer, hour_update_proc);
-  layer_add_child(minute_layer, hour_layer);
-  index = generate_inverter_layers(index, hour_layer);
+  time_paths[HOUR] = gpath_create(&time_infos[HOUR]);
+  layer_set_update_proc(time_layers[HOUR], hour_update_proc);
+  layer_add_child(time_layers[MINUTE], time_layers[HOUR]);
+  index = generate_inverter_layers(index, time_layers[HOUR]);
 
   time_t t = time(NULL);
   tick_handler(localtime(&t), SECOND_UNIT | HOUR_UNIT | MINUTE_UNIT);
 }
 
 static void window_unload(Window *window) {
-  layer_destroy(second_layer);
-  layer_destroy(minute_layer);
-  layer_destroy(hour_layer);
-  gpath_destroy(second_path);
-  gpath_destroy(hour_path);
-  gpath_destroy(minute_path);
+  for (int i = 0; i < TIME_SIZE; i++) {
+    layer_destroy(time_layers[i]);
+    gpath_destroy(time_paths[i]);
+  }
+
   for (int i = 0; i < INVERT_LAYER_COUNT; i++) {
     inverter_layer_destroy(invert_layers[i]);
   }
